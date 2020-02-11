0fa4d8c9bffbc677185457b2e258aaeecfcb138b
Elifinanc <58734605+Elifinanc@users.noreply.github.com>
2020-02-11 15:21:12 +0100

    Add files via upload

---
+++ IExecutable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueteSolid3DIP
+{
+    public interface IExecutable
+    {
+        public bool Exited { get; set; }
+        public Command PromptCommand();
+        public string Prompt();
+        public void ExecuteCommand(Command command);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QueteSolid3DIP
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            IExecutable terminal = new Terminal();
+            while (!terminal.Exited)
+            {
+                Command command = terminal.PromptCommand();
+                terminal.ExecuteCommand(command);
+            }
+        }
+    }
+}
